7f3fbcc62c1037d8d9e8f9b99e3e9ff2a07d011b
David <dgeffers@outlook.de>
2025-03-06 14:03:17 +0100

    simulating access modifier

--- batDetector/Celldata.py
+++ batDetector/Celldata.py
@@ -16,65 +16,65 @@ class Celldata:
         @param curve: Dataframe
         @type curve: pandas.DataFrame
         """
-        self.cell_composition = comp
-        self.curve = curve
-        self.is_half_cell = is_half_cell
-        self.is_neg = is_neg
+        self._cell_composition = comp
+        self._curve = curve
+        self._is_half_cell = is_half_cell
+        self._is_neg = is_neg
 
         if not is_half_cell:
             self.rename_columns([SOC, VOLTAGE])
-            self.curve[SOC] = self.norm_curve(SOC)
+            self._curve[SOC] = self.norm_curve(SOC)
         else:
             self.rename_columns([LITHIATION, VOLTAGE])
-            self.curve[LITHIATION] = self.norm_curve(LITHIATION)
+            self._curve[LITHIATION] = self.norm_curve(LITHIATION)
 
-        if self.is_half_cell:
+        if self._is_half_cell:
             self.check_orientation()
 
     def plot_data(self):
-        if not self.is_half_cell:
-            self.curve.plot(x=SOC, y=VOLTAGE)
+        if not self._is_half_cell:
+            self._curve.plot(x=SOC, y=VOLTAGE)
         else:
-            self.curve.plot(x=LITHIATION, y=VOLTAGE)
+            self._curve.plot(x=LITHIATION, y=VOLTAGE)
 
         plt.show()
         plt.close()
 
     def rename_columns(self, new_columns: list[str]):
-        old_cols = self.curve.columns
+        old_cols = self._curve.columns
         for i in range(len(old_cols)):
-            self.curve.rename(columns={old_cols[i]: new_columns[i]}, inplace=True)
+            self._curve.rename(columns={old_cols[i]: new_columns[i]}, inplace=True)
 
     def check_orientation(self):
        self.ocv_flip_dataframe()
 
     def get_composition(self):
-        return self.cell_composition
+        return self._cell_composition
 
     def get_data(self):
-        return self.curve
+        return self._curve
 
     def set_data(self, dataframe: pd.DataFrame):
-        self.curve = dataframe
+        self._curve = dataframe
 
     def set_composition(self, comp: str):
-        self.cell_composition = comp
+        self._cell_composition = comp
 
     def ocv_flip_dataframe(self):
-        start = self.curve[VOLTAGE].iat[0]
-        end = self.curve[VOLTAGE].iat[-1]
-        if self.is_neg:
+        start = self._curve[VOLTAGE].iat[0]
+        end = self._curve[VOLTAGE].iat[-1]
+        if self._is_neg:
             if start < end:
-                self.curve[VOLTAGE] = self.curve[VOLTAGE].values[::-1]
+                self._curve[VOLTAGE] = self._curve[VOLTAGE].values[::-1]
         else:
             if start > end:
-                self.curve[VOLTAGE] = self.curve[VOLTAGE].values[::-1]
+                self._curve[VOLTAGE] = self._curve[VOLTAGE].values[::-1]
 
     def get_is_halfcell(self):
-        return self.is_half_cell
+        return self._is_half_cell
 
     def get_is_neg(self):
-        return self.is_neg
+        return self._is_neg
 
     def norm_curve(self,colm):
-        return (self.curve[colm] - self.curve[colm].min()) / (self.curve[colm].max() - self.curve[colm].min())
+        return (self._curve[colm] - self._curve[colm].min()) / (self._curve[colm].max() - self._curve[colm].min())
